d80ae415b46576cdaa6446ec4d4904828fa2fc06
Aybekka <60847060+Aybekka@users.noreply.github.com>
2026-02-04 18:48:32 +0300

    Refactor vite.config.js for base and global settings
    Updated Vite configuration for base path and global definition.

--- vite.config.js
+++ vite.config.js
@@ -3,46 +3,17 @@ import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
-
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
-      },
-      outDir: '../dist',
-      emptyOutDir: true,
-    },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+export default defineConfig({
+  // ÖNEMLİ: Buradaki repo-adi kısmını GitHub'daki klasör isminle değiştir!
+  // Örneğin: base: '/goit-js-hw-09/'
+  base: '/repo-adi/', 
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  // global hatasını önlemek için (Gerekirse)
+  define: {
+    global: 'window',
+  },
 });
